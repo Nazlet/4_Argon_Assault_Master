59987daae1765d3cbc11ce29f98ee6e700a8260e
Justin <justincoutts@icloud.com>
2020-09-08 12:06:36 +0100

    AA_Roll_Pitch_Yaw

--- 4_Argon_Assault/Assets/Player.cs
+++ 4_Argon_Assault/Assets/Player.cs
@@ -5,7 +5,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRange = 12f;
+
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRangeMin = -12f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRangeMax = 12f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +22,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
-        print(xOffsetThisFrame);
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, +xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, yRangeMin, yRangeMax);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
     }
 }
