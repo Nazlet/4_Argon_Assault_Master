cd6364f0c9f9e7a1f6ccd99efc3c2f8321766ea0
Justin <justincoutts@icloud.com>
2020-09-16 11:30:51 +0100

    AA_103_Scripts_Add_Components

---
+++ 4_Argon_Assault/Assets/Scripts/Enemies.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
