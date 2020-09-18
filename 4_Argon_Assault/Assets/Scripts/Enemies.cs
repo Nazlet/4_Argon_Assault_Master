403e3f0ba66fc1c64a58524c90bad0a04845e52c
Justin <justincoutts@icloud.com>
2020-09-18 13:03:57 +0100

    AA_108_Intro_Encapsulation

--- 4_Argon_Assault/Assets/Scripts/Enemies.cs
+++ 4_Argon_Assault/Assets/Scripts/Enemies.cs
@@ -7,9 +7,18 @@ public class Enemies : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
@@ -22,6 +31,7 @@ public class Enemies : MonoBehaviour
     }
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
