e874ce642ee51ca7830bff98c96e877696372085
Justin <justincoutts@icloud.com>
2020-09-29 10:11:44 +0100

    PT_8_Guns_BoxCollider_Explosion Check BoxCollider shape if collisions don't work

---
+++ 4_Argon_Assault/Assets/Scripts/PTCollisionHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //ok as long as this is the only script that loads scenes
+
+public class PTCollisionHandler : MonoBehaviour
+{
+    [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene() // string referenced
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
